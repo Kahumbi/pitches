b01725cde7f0587d39660ebe4d389e32c5b7e5ca
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-05-09 18:29:58 +0300

    Add a logout urlfor login page

--- pitches/routes.py
+++ pitches/routes.py
@@ -2,7 +2,7 @@ from flask import  render_template, url_for, flash, redirect
 from pitches import app, db, bcrypt
 from pitches.forms import RegistrationForm, LoginForm 
 from pitches.models import User, Pitch
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user
 
 
 posts = [
@@ -33,6 +33,8 @@ def about():
 
 @app.route("/register" , methods= ['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -45,6 +47,8 @@ def register():
 
 @app.route("/login" , methods= ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -54,3 +58,9 @@ def login():
         else:    
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
